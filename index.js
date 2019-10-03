047349485eebaf6e68f8844ee8897ad41f0f13da
Luiz Felipe <felipealvesalp@gmail.com>
2019-10-02 23:24:43 -0300

    Connection mongoDB

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 //Iniciando o APP
 const app = express()
+
+//Iniciando o DB
+mongoose.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser: true, useUnifiedTopology: true})
 
 //Primeira Rota (Rota raiz)
 app.get('/', (req, res) => {
